429669e76092f10c969a0a9d67f16fe3248e3690
fernandojunior <fernandojr.ifcg@live.com>
2017-01-24 12:09:35 -0300

    Minor dix

--- src/main/js/components/comment/AddComment.js
+++ src/main/js/components/comment/AddComment.js
@@ -31,7 +31,7 @@ class AddComment extends React.Component {
 
   render() {
     return (
-      <form onSubmit={ this.handleSaveComment }>
+      <form onSubmit={ e => this.handleSaveComment(e) }>
         <h1>Add Comment</h1>
         <div className="form-group">
           <label htmlFor="author">Author:</label>
