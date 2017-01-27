e8b69e5c55c05677b63a58e6356804ef7216b958
fernandojunior <fernandojr.ifcg@live.com>
2017-01-27 11:49:39 -0300

    Polish MatchWhenAuthorized component

--- src/main/js/containers/MatchWhenAuthorized.js
+++ src/main/js/containers/MatchWhenAuthorized.js
@@ -3,25 +3,16 @@ import React, { PropTypes } from 'react';
 import { Match, Redirect } from 'react-router';
 import { connect } from 'react-redux';
 
+const RedirectToSignIn = props => (
+  <Redirect to={{ pathname: '/signin', state: { from: props.location } }} />
+);
+
 const MatchWhenAuthorized = ({ component: Component, signedIn, ...rest }) => {
   const onComponentRender = props => (
-    signedIn
-      ? <Component {...props} />
-      : (<Redirect
-        to={{
-          pathname: '/signin',
-          // eslint-disable-next-line react/prop-types
-          state: { from: props.location }
-        }}
-      />)
+    signedIn ? <Component {...props} /> : <RedirectToSignIn { ...props } />
   );
 
   return <Match {...rest} render={onComponentRender} />;
-};
-
-MatchWhenAuthorized.propTypes = {
-  component: PropTypes.func,
-  signedIn: PropTypes.bool
 };
 
 function mapStateToProps(state) {
