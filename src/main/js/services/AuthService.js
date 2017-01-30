5e14788fb1eca219a55391c1a3181c75f4c37afe
fernandojunior <fernandojr.ifcg@live.com>
2017-01-30 11:21:28 -0300

    Add flow: AuthService

--- src/main/js/services/AuthService.js
+++ src/main/js/services/AuthService.js
@@ -1,13 +1,14 @@
+/* @flow */
 import axios from 'axios';
 
 class AuthService {
 
-  signIn(username, password) {
+  signIn(username : string, password: string) : Object {
     const data = `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`;
       return axios.post('/api/signin', data);
   }
 
-  signOut() {
+  signOut() : Object {
     return axios.post('/api/signout');
   }
 
